535ea4de58e162649a071c60f418c881568a8349
Curtis Larson <quackware@gmail.com>
2022-11-26 08:55:46 -0500

    feat: add resume

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # curtislarson.dev
+
+## Development
+
+```zsh
+pnpm run dev
+```
+
+## Building
+
+```zsh
+pnpm run build
+```
+
+## TODO
+
+- [ ] Move to Deno when I have a chance
--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,22 +1,29 @@
-import Gh from "img/gh.png";
-
 export default function () {
   return (
     <div class="hero min-h-screen bg-base-200">
-      <div class="hero-content flex-col text-center">
-        <div class="max-w-md">
-          <h1 class="text-6xl font-bold">Curtis Larson</h1>
-          <h2 class="text-2xl py-4">Freelance Software Developer</h2>
-          <p>I build stuff while traveling around the world.</p>
-          <p class="text-primary py-2">
-            <a href="mailto:hi@curtislarson.dev">hi@curtislarson.dev</a>
+      <div class="hero-content flex flex-col text-center">
+        <h1 class="text-6xl font-bold">Curtis Larson</h1>
+        <h2 class="text-2xl">Freelance Software Developer</h2>
+        <p>I build stuff while traveling around the world.</p>
+        <div class="divider"></div>
+        <div class="flex flex-row">
+          <p class="text-primary py-2 px-2">
+            <a href="mailto:hi@curtislarson.dev" alt="Contact" class="btn btn-primary">
+              Contact
+            </a>
+          </p>
+          <p class="text-primary py-2 px-2">
+            <a href="/resume.pdf" alt="Resume" class="btn btn-primary">
+              Resume
+            </a>
+          </p>
+          <p class="text-primary py-2 px-2">
+            <a href="https://github.com/curtislarson" alt="Github" class="btn btn-primary">
+              Github
+            </a>
           </p>
         </div>
-        <div class="items-center py-2">
-          <a href="https://github.com/curtislarson">
-            <img class="object-scale-down h-10 w-10" alt="github" src={Gh} />
-          </a>
-        </div>
+        <div class="flex flex-row"></div>
       </div>
     </div>
   );
