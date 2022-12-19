a07d56ed0bc30fe0858bc0c8f811589676d81882
Curtis Larson <quackware@gmail.com>
2022-12-18 19:54:06 -0500

    fix: postcss config file not required when using vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import preact from "@preact/preset-vite";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
   plugins: [
     preact(),
     tsconfigPaths(),
