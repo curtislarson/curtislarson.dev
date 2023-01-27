976f3a20c08710dceafc95d09b003b9fa6c66a5e
Curtis Larson <quackware@gmail.com>
2023-01-26 22:29:15 -0500

    fix: fine ill use hashrouter, fixes 404 for direct navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path={"/"} element={<Home />} />
           <Route path={"/projects"} element={<Projects />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
