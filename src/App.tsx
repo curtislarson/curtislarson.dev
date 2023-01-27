2adec52d39e2a5f2541de0e54c94854d2312aa7e
Curtis Larson <quackware@gmail.com>
2023-01-26 22:15:05 -0500

    fix: i guess lazy routes dont work on github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,16 @@
 import { BrowserRouter, Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
-import { JSX, lazy, Suspense } from "preact/compat";
 import Layout from "./Layout";
-
-type ImportComponent = { default: (...args: any[]) => JSX.Element };
-
-function lazyRoute(path: string, component: Promise<ImportComponent>, props: Record<string, unknown> = {}) {
-  const Component = lazy(() => component);
-  return (
-    <Route
-      path={path}
-      element={
-        <Suspense fallback={<div></div>}>
-          <Component {...props} />
-        </Suspense>
-      }
-    />
-  );
-}
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          {lazyRoute("/", import("./pages/Home"))}
-          {lazyRoute("/projects", import("./pages/Projects"))}
-          {lazyRoute("/projects/:projectId", import("./pages/Project"))}
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/projects"} element={<Projects />} />
         </Route>
       </Routes>
     </BrowserRouter>
