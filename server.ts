f8acafb91ddbf1665a3c6adcbc643f6959696e4a
Curtis Larson <quackware@gmail.com>
2023-12-19 16:20:33 -0500

    fix: make sure we can load projects page on hard refresh

--- server.ts
+++ server.ts
@@ -5,8 +5,12 @@ import { serveStatic } from "https://deno.land/x/hono@v3.1.5/middleware.ts";
 import { Hono } from "https://deno.land/x/hono@v3.1.5/mod.ts";
 
 const app = new Hono();
-app.get("/", serveStatic({ path: "./dist/index.html" }));
-app.get("*", serveStatic({ root: "./dist" }));
+
+app.get("/assets/*", serveStatic({ root: "./dist/" }));
+app.get("/quack.png", serveStatic({ path: "./dist/quack.png" }));
+app.get("/favicon.ico", serveStatic({ path: "./dist/favicon.ico" }));
+
+app.get("*", serveStatic({ path: "./dist/index.html" }));
 
 serve(app.fetch, {
   hostname: Deno.env.get("HOSTNAME") ?? "localhost",
