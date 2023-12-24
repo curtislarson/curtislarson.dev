757936ccf9646025ebb6581e80983f8f1f55f56f
Curtis Larson <quackware@gmail.com>
2023-12-24 11:32:14 -0500

    chore: small fixes to get tailwind autocomplete working again

---
+++ packages/www/tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./packages/**/*.{js,ts,jsx,tsx}"],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["dracula"],
+    darkTheme: "dark",
+  },
+};
