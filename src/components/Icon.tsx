d7823c113cffc4a81ac50f704bd80fa6185afc84
Curtis Larson <quackware@gmail.com>
2023-12-24 08:56:09 -0500

    feat: update package dependencies and tweak navbar styles again

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -7,9 +7,9 @@ export default function Icon({ src, tooltip }: IconProps) {
   if (tooltip) {
     return (
       <div className={`tooltip tooltip-bottom normal-case`} data-tip={tooltip}>
-        <img src={src} className="h-8 w-8" />
+        <img src={src} className="h-7 w-7" />
       </div>
     );
   }
-  return <img src={src} className="h-8 w-8" />;
+  return <img src={src} className="h-7 w-7" />;
 }
