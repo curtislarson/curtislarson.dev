b243516a4b1f92867102c5d6455822fd76de19ae
Curtis Larson <quackware@gmail.com>
2023-12-29 09:25:39 -0500

    feat: auto generate guides sidebar based on files in guides directory

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,7 +1,39 @@
+import { readdirSync } from "node:fs";
+import { basename, join } from "node:path";
 import { DefaultTheme, defineConfig } from "vitepress";
 
+function toTitleCase(str: string) {
+  return str.toLowerCase().replace(/(?:^|[\s-/])\w/g, function (match) {
+    return match.toUpperCase();
+  });
+}
+
+function getGuidesSidebarItems() {
+  const guidesPath = new URL("../src/guides", import.meta.url).pathname;
+  const allGuides = readdirSync(guidesPath);
+  const sidebarItems = allGuides.map((path) => {
+    const baseLink = basename(path, ".md");
+    if (baseLink === "index") {
+      return null;
+    }
+    const link = join("/guides/", baseLink);
+    const text = toTitleCase(baseLink).replaceAll("-", " ");
+    return {
+      link,
+      text,
+    };
+  }).filter((v): v is NonNullable<typeof v> => v != null);
+
+  console.log({ sidebarItems });
+
+  return sidebarItems;
+}
+
 const sidebar: DefaultTheme.Sidebar = {
-  "/guides/": [],
+  "/guides/": [{
+    text: "Guides",
+    items: getGuidesSidebarItems(),
+  }],
 };
 
 export default defineConfig({
