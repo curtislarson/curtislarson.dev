330d4f00a0113a6ae7bec8f504d0fc4a70a11fe2
Curtis Larson <quackware@gmail.com>
2023-12-29 10:02:59 -0500

    chore: update github link

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -50,7 +50,7 @@ export default defineConfig({
     sidebar,
 
     socialLinks: [
-      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+      { icon: "github", link: "https://github.com/curtislarson" },
     ],
   },
 });
